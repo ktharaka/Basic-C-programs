34c303e95b9e2a21c45a0c25e25599d38120b27c
Kasun Tharaka Gunarathne <ktharaka56@gmail.com>
2020-01-25 05:41:09 +0530

    Add files via upload

---
+++ printInt.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+
+ int testInteger = 5;
+ printf("Integer = %d\n", testInteger);
+ return 0;
+}
---
+++ readFloat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+ float testNumber;
+	        
+ printf("Enter a number: ");
+ scanf("%f", &testNumber);
+ printf("You Entered : %f\n", testNumber);
+ return 0;
+}
